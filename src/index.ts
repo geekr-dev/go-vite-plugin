e64f1e38d145803192a60da0301083e9e6274c68
geekr <geekr@hey.com>
2023-05-25 12:23:32 +0800

    modify code to adjust go app

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import fs from 'fs'
 import { AddressInfo } from 'net'
-import os from 'os'
 import { fileURLToPath } from 'url'
 import path from 'path'
 import colors from 'picocolors'
@@ -11,10 +10,10 @@ interface PluginConfig {
     /**
      * The path or paths of the entry points to compile.
      */
-    input: string|string[]
+    input: string | string[]
 
     /**
-     * Laravel's public directory.
+     * App's public directory.
      *
      * @default 'public'
      */
@@ -37,7 +36,7 @@ interface PluginConfig {
     /**
      * The path of the SSR entry point.
      */
-    ssr?: string|string[]
+    ssr?: string | string[]
 
     /**
      * The directory where the SSR bundle should be written.
@@ -52,14 +51,14 @@ interface PluginConfig {
      * {@link https://github.com/ElMassimo/vite-plugin-full-reload}
      * @default false
      */
-    refresh?: boolean|string|string[]|RefreshConfig|RefreshConfig[]
+    refresh?: boolean | string | string[] | RefreshConfig | RefreshConfig[]
 
     /**
      * Utilise the valet TLS certificates.
      *
      * @default false
      */
-    valetTls?: string|boolean,
+    valetTls?: string | boolean,
 
     /**
      * Transform the code while serving.
@@ -72,40 +71,38 @@ interface RefreshConfig {
     config?: FullReloadConfig,
 }
 
-interface LaravelPlugin extends Plugin {
+interface GoPlugin extends Plugin {
     config: (config: UserConfig, env: ConfigEnv) => UserConfig
 }
 
-type DevServerUrl = `${'http'|'https'}://${string}:${number}`
+type DevServerUrl = `${'http' | 'https'}://${string}:${number}`
 
 let exitHandlersBound = false
 
 export const refreshPaths = [
-    'app/View/Components/**',
+    'internal/view/components/**',
     'resources/views/**',
     'resources/lang/**',
-    'lang/**',
-    'routes/**',
 ]
 
 /**
- * Laravel plugin for Vite.
+ * Go plugin for Vite.
  *
  * @param config - A config object or relative path(s) of the scripts to be compiled.
  */
-export default function laravel(config: string|string[]|PluginConfig): [LaravelPlugin, ...Plugin[]]  {
+export default function go(config: string | string[] | PluginConfig): [GoPlugin, ...Plugin[]] {
     const pluginConfig = resolvePluginConfig(config)
 
     return [
-        resolveLaravelPlugin(pluginConfig),
+        resolveGoPlugin(pluginConfig),
         ...resolveFullReloadConfig(pluginConfig) as Plugin[],
     ];
 }
 
 /**
- * Resolve the Laravel Plugin configuration.
+ * Resolve the Go Plugin configuration.
  */
-function resolveLaravelPlugin(pluginConfig: Required<PluginConfig>): LaravelPlugin {
+function resolveGoPlugin(pluginConfig: Required<PluginConfig>): GoPlugin {
     let viteDevServerUrl: DevServerUrl
     let resolvedConfig: ResolvedConfig
 
@@ -114,14 +111,14 @@ function resolveLaravelPlugin(pluginConfig: Required<PluginConfig>): LaravelPlug
     };
 
     return {
-        name: 'laravel',
+        name: 'go',
         enforce: 'post',
         config: (userConfig, { command, mode }) => {
-            const ssr = !! userConfig.build?.ssr
+            const ssr = !!userConfig.build?.ssr
             const env = loadEnv(mode, userConfig.envDir || process.cwd(), '')
             const assetUrl = env.ASSET_URL ?? ''
             const serverConfig = command === 'serve'
-                ? (resolveValetServerConfig(pluginConfig.valetTls) ?? resolveEnvironmentServerConfig(env))
+                ? resolveEnvironmentServerConfig(env)
                 : undefined
 
             ensureCommandShouldRunInEnvironment(command, env)
@@ -138,8 +135,8 @@ function resolveLaravelPlugin(pluginConfig: Required<PluginConfig>): LaravelPlug
                     assetsInlineLimit: userConfig.build?.assetsInlineLimit ?? 0,
                 },
                 server: {
-                    origin: userConfig.server?.origin ?? '__laravel_vite_placeholder__',
-                    ...(process.env.LARAVEL_SAIL ? {
+                    origin: userConfig.server?.origin ?? '__go_vite_placeholder__',
+                    ...(process.env.DOCKER ? {
                         host: userConfig.server?.host ?? '0.0.0.0',
                         port: userConfig.server?.port ?? (env.VITE_PORT ? parseInt(env.VITE_PORT) : 5173),
                         strictPort: userConfig.server?.strictPort ?? true,
@@ -180,7 +177,7 @@ function resolveLaravelPlugin(pluginConfig: Required<PluginConfig>): LaravelPlug
         },
         transform(code) {
             if (resolvedConfig.command === 'serve') {
-                code = code.replace(/__laravel_vite_placeholder__/g, viteDevServerUrl)
+                code = code.replace(/__go_vite_placeholder__/g, viteDevServerUrl)
 
                 return pluginConfig.transformOnServe(code, viteDevServerUrl)
             }
@@ -192,20 +189,20 @@ function resolveLaravelPlugin(pluginConfig: Required<PluginConfig>): LaravelPlug
             server.httpServer?.once('listening', () => {
                 const address = server.httpServer?.address()
 
-                const isAddressInfo = (x: string|AddressInfo|null|undefined): x is AddressInfo => typeof x === 'object'
+                const isAddressInfo = (x: string | AddressInfo | null | undefined): x is AddressInfo => typeof x === 'object'
                 if (isAddressInfo(address)) {
                     viteDevServerUrl = resolveDevServerUrl(address, server.config)
                     fs.writeFileSync(pluginConfig.hotFile, viteDevServerUrl)
 
                     setTimeout(() => {
-                        server.config.logger.info(`\n  ${colors.red(`${colors.bold('LARAVEL')} ${laravelVersion()}`)}  ${colors.dim('plugin')} ${colors.bold(`v${pluginVersion()}`)}`)
+                        server.config.logger.info(`\n  ${colors.red(`${colors.bold('GO')} ${goVersion()}`)}  ${colors.dim('plugin')} ${colors.bold(`v${pluginVersion()}`)}`)
                         server.config.logger.info('')
                         server.config.logger.info(`  ${colors.green('➜')}  ${colors.bold('APP_URL')}: ${colors.cyan(appUrl.replace(/:(\d+)/, (_, port) => `:${colors.bold(port)}`))}`)
                     }, 100)
                 }
             })
 
-            if (! exitHandlersBound) {
+            if (!exitHandlersBound) {
                 const clean = () => {
                     if (fs.existsSync(pluginConfig.hotFile)) {
                         fs.rmSync(pluginConfig.hotFile)
@@ -238,21 +235,9 @@ function resolveLaravelPlugin(pluginConfig: Required<PluginConfig>): LaravelPlug
 /**
  * Validate the command can run in the given environment.
  */
-function ensureCommandShouldRunInEnvironment(command: 'build'|'serve', env: Record<string, string>): void {
-    if (command === 'build' || env.LARAVEL_BYPASS_ENV_CHECK === '1') {
+function ensureCommandShouldRunInEnvironment(command: 'build' | 'serve', env: Record<string, string>): void {
+    if (command === 'build' || env.GO_BYPASS_ENV_CHECK === '1') {
         return;
-    }
-
-    if (typeof env.LARAVEL_VAPOR !== 'undefined') {
-        throw Error('You should not run the Vite HMR server on Vapor. You should build your assets for production instead. To disable this ENV check you may set LARAVEL_BYPASS_ENV_CHECK=1');
-    }
-
-    if (typeof env.LARAVEL_FORGE !== 'undefined') {
-        throw Error('You should not run the Vite HMR server in your Forge deployment script. You should build your assets for production instead. To disable this ENV check you may set LARAVEL_BYPASS_ENV_CHECK=1');
-    }
-
-    if (typeof env.LARAVEL_ENVOYER !== 'undefined') {
-        throw Error('You should not run the Vite HMR server in your Envoyer hook. You should build your assets for production instead. To disable this ENV check you may set LARAVEL_BYPASS_ENV_CHECK=1')
     }
 
     if (typeof env.CI !== 'undefined') {
@@ -261,20 +246,14 @@ function ensureCommandShouldRunInEnvironment(command: 'build'|'serve', env: Reco
 }
 
 /**
- * The version of Laravel being run.
+ * The version of Go being run.
  */
-function laravelVersion(): string {
-    try {
-        const composer = JSON.parse(fs.readFileSync('composer.lock').toString())
-
-        return composer.packages?.find((composerPackage: {name: string}) => composerPackage.name === 'laravel/framework')?.version ?? ''
-    } catch {
-        return ''
-    }
+function goVersion(): string {
+    return '1.20.4'
 }
 
 /**
- * The version of the Laravel Vite plugin being run.
+ * The version of the Go Vite plugin being run.
  */
 function pluginVersion(): string {
     try {
@@ -287,9 +266,9 @@ function pluginVersion(): string {
 /**
  * Convert the users configuration into a standard structure with defaults.
  */
-function resolvePluginConfig(config: string|string[]|PluginConfig): Required<PluginConfig> {
+function resolvePluginConfig(config: string | string[] | PluginConfig): Required<PluginConfig> {
     if (typeof config === 'undefined') {
-        throw new Error('laravel-vite-plugin: missing configuration.')
+        throw new Error('go-vite-plugin: missing configuration.')
     }
 
     if (typeof config === 'string' || Array.isArray(config)) {
@@ -297,14 +276,14 @@ function resolvePluginConfig(config: string|string[]|PluginConfig): Required<Plu
     }
 
     if (typeof config.input === 'undefined') {
-        throw new Error('laravel-vite-plugin: missing configuration for "input".')
+        throw new Error('go-vite-plugin: missing configuration for "input".')
     }
 
     if (typeof config.publicDirectory === 'string') {
         config.publicDirectory = config.publicDirectory.trim().replace(/^\/+/, '')
 
         if (config.publicDirectory === '') {
-            throw new Error('laravel-vite-plugin: publicDirectory must be a subdirectory. E.g. \'public\'.')
+            throw new Error('go-vite-plugin: publicDirectory must be a subdirectory. E.g. \'public\'.')
         }
     }
 
@@ -312,7 +291,7 @@ function resolvePluginConfig(config: string|string[]|PluginConfig): Required<Plu
         config.buildDirectory = config.buildDirectory.trim().replace(/^\/+/, '').replace(/\/+$/, '')
 
         if (config.buildDirectory === '') {
-            throw new Error('laravel-vite-plugin: buildDirectory must be a subdirectory. E.g. \'build\'.')
+            throw new Error('go-vite-plugin: buildDirectory must be a subdirectory. E.g. \'build\'.')
         }
     }
 
@@ -341,13 +320,13 @@ function resolvePluginConfig(config: string|string[]|PluginConfig): Required<Plu
  * Resolve the Vite base option from the configuration.
  */
 function resolveBase(config: Required<PluginConfig>, assetUrl: string): string {
-    return assetUrl + (! assetUrl.endsWith('/') ? '/' : '') + config.buildDirectory + '/'
+    return assetUrl + (!assetUrl.endsWith('/') ? '/' : '') + config.buildDirectory + '/'
 }
 
 /**
  * Resolve the Vite input path from the configuration.
  */
-function resolveInput(config: Required<PluginConfig>, ssr: boolean): string|string[]|undefined {
+function resolveInput(config: Required<PluginConfig>, ssr: boolean): string | string[] | undefined {
     if (ssr) {
         return config.ssr
     }
@@ -358,7 +337,7 @@ function resolveInput(config: Required<PluginConfig>, ssr: boolean): string|stri
 /**
  * Resolve the Vite outDir path from the configuration.
  */
-function resolveOutDir(config: Required<PluginConfig>, ssr: boolean): string|undefined {
+function resolveOutDir(config: Required<PluginConfig>, ssr: boolean): string | undefined {
     if (ssr) {
         return config.ssrOutputDirectory
     }
@@ -366,16 +345,16 @@ function resolveOutDir(config: Required<PluginConfig>, ssr: boolean): string|und
     return path.join(config.publicDirectory, config.buildDirectory)
 }
 
-function resolveFullReloadConfig({ refresh: config }: Required<PluginConfig>): PluginOption[]{
+function resolveFullReloadConfig({ refresh: config }: Required<PluginConfig>): PluginOption[] {
     if (typeof config === 'boolean') {
         return [];
     }
 
     if (typeof config === 'string') {
-        config = [{ paths: [config]}]
+        config = [{ paths: [config] }]
     }
 
-    if (! Array.isArray(config)) {
+    if (!Array.isArray(config)) {
         config = [config]
     }
 
@@ -388,7 +367,7 @@ function resolveFullReloadConfig({ refresh: config }: Required<PluginConfig>): P
 
         /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
         /** @ts-ignore */
-        plugin.__laravel_plugin_config = c
+        plugin.__go_plugin_config = c
 
         return plugin
     })
@@ -417,7 +396,6 @@ function resolveDevServerUrl(address: AddressInfo, config: ResolvedConfig): DevS
 function isIpv6(address: AddressInfo): boolean {
     return address.family === 'IPv6'
         // In node >=18.0 <18.4 this was an integer value. This was changed in a minor version.
-        // See: https://github.com/laravel/vite-plugin/issues/103
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore-next-line
         || address.family === 6;
@@ -428,11 +406,11 @@ function isIpv6(address: AddressInfo): boolean {
  *
  * @see https://vitejs.dev/guide/ssr.html#ssr-externals
  */
-function noExternalInertiaHelpers(config: UserConfig): true|Array<string|RegExp> {
+function noExternalInertiaHelpers(config: UserConfig): true | Array<string | RegExp> {
     /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
     /* @ts-ignore */
-    const userNoExternal = (config.ssr as SSROptions|undefined)?.noExternal
-    const pluginNoExternal = ['laravel-vite-plugin']
+    const userNoExternal = (config.ssr as SSROptions | undefined)?.noExternal
+    const pluginNoExternal = ['go-vite-plugin']
 
     if (userNoExternal === true) {
         return true
@@ -455,18 +433,18 @@ function resolveEnvironmentServerConfig(env: Record<string, string>): {
     hmr?: { host: string }
     host?: string,
     https?: { cert: Buffer, key: Buffer }
-}|undefined {
-    if (! env.VITE_DEV_SERVER_KEY && ! env.VITE_DEV_SERVER_CERT) {
+} | undefined {
+    if (!env.VITE_DEV_SERVER_KEY && !env.VITE_DEV_SERVER_CERT) {
         return
     }
 
-    if (! fs.existsSync(env.VITE_DEV_SERVER_KEY) || ! fs.existsSync(env.VITE_DEV_SERVER_CERT)) {
+    if (!fs.existsSync(env.VITE_DEV_SERVER_KEY) || !fs.existsSync(env.VITE_DEV_SERVER_CERT)) {
         throw Error(`Unable to find the certificate files specified in your environment. Ensure you have correctly configured VITE_DEV_SERVER_KEY: [${env.VITE_DEV_SERVER_KEY}] and VITE_DEV_SERVER_CERT: [${env.VITE_DEV_SERVER_CERT}].`)
     }
 
     const host = resolveHostFromEnv(env)
 
-    if (! host) {
+    if (!host) {
         throw Error(`Unable to determine the host from the environment's APP_URL: [${env.APP_URL}].`)
     }
 
@@ -483,60 +461,12 @@ function resolveEnvironmentServerConfig(env: Record<string, string>): {
 /**
  * Resolve the host name from the environment.
  */
-function resolveHostFromEnv(env: Record<string, string>): string|undefined
-{
+function resolveHostFromEnv(env: Record<string, string>): string | undefined {
     try {
         return new URL(env.APP_URL).host
     } catch {
         return
     }
-}
-
-
-/**
- * Resolve the valet server config for the given host.
- */
-function resolveValetServerConfig(host: string|boolean): {
-    hmr?: { host: string }
-    host?: string,
-    https?: { cert: Buffer, key: Buffer }
-}|undefined {
-    if (host === false) {
-        return
-    }
-
-    host = host === true ? resolveValetHost() : host
-
-    const keyPath = path.resolve(os.homedir(), `.config/valet/Certificates/${host}.key`)
-    const certPath = path.resolve(os.homedir(), `.config/valet/Certificates/${host}.crt`)
-
-    if (! fs.existsSync(keyPath) || ! fs.existsSync(certPath)) {
-        throw Error(`Unable to find Valet certificate files for your host [${host}]. Ensure you have run "valet secure".`)
-    }
-
-    return {
-        hmr: { host },
-        host,
-        https: {
-            key: fs.readFileSync(keyPath),
-            cert: fs.readFileSync(certPath),
-        },
-    }
-}
-
-/**
- * Resolve the valet valet host for the current directory.
- */
-function resolveValetHost(): string {
-    const configPath = os.homedir() + `/.config/valet/config.json`
-
-    if (! fs.existsSync(configPath)) {
-        throw Error('Unable to find the Valet configuration file. You will need to manually specify the host in the `valetTls` configuration option.')
-    }
-
-    const config: { tld: string } = JSON.parse(fs.readFileSync(configPath, 'utf-8'))
-
-    return path.basename(process.cwd()) + '.' + config.tld
 }
 
 /**
